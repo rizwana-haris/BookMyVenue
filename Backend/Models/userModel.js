fe65247b6487149de407e171fafbfbec3ed482d7
rizwana haris <rizwanaharis1@gmail.com>
2026-06-01 19:06:03 +0400

    user register updates

--- Backend/Models/userModel.js
+++ Backend/Models/userModel.js
@@ -14,7 +14,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        minlength: 6
+        minlength: 6,
+        select: false
     },
     phone: {
         type: String,
