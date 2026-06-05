9cae8da205fbd23e03fd5b0fcadc41d3da94417a
hawwarafiya <hawwarafiya@gmail.com>
2026-06-05 10:18:04 -0700

    Login page UI compelted with role section and signup link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/NavbarNew";
+import Login from "./Login";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      
+      <Login />
     </div>
   );
 }
