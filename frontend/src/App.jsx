10b9a215ba948a6766d4b52e604d9987c7670d06
hawwarafiya <hawwarafiya@gmail.com>
2026-06-08 23:16:15 -0700

    Address PR review comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavbarNew";
-import Login from "./Login";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Login />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
